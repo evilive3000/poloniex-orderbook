665ba6c9b6acf691d984f16f9530aab9bab8e0b2
evilive3000 <evilive3000@gmail.com>
2017-03-21 11:08:11 +0200

    Update examples

---
+++ examples/multipairs.js
@@ -0,0 +1,29 @@
+const PoloManager = require('../');
+const poloman = new PoloManager().connect();
+
+poloman.on('error', err => console.log(err));
+
+const pairs = ['BTC_ETH', 'USDT_ETH', 'USDT_BTC'];
+const markets = {};
+
+poloman.on('change', info => {
+  const {side, channel} = info;
+  const top5 = markets[channel][side].slice(0, 5);
+  console.log(`${channel}.${side}`);
+  console.log(top5);
+});
+
+for (const pair of pairs) {
+  markets[pair] = poloman.market(pair);
+}
+
+// 5 seconds later
+setTimeout(() => {
+  poloman.remove('USDT_ETH');
+  poloman.remove('USDT_BTC');
+}, 5000);
+
+// 10 seconds later
+setTimeout(() => {
+  poloman.disconnect();
+}, 10000);
---
+++ examples/singlepair.js
@@ -0,0 +1,20 @@
+const PoloManager = require('../');
+const poloman = new PoloManager().connect();
+
+poloman.on('error', err => console.log(err));
+
+poloman.on('change', info => {
+  const {channel, side} = info;
+  const market = poloman.market(channel);
+  const top5 = market[side].slice(0, 5);
+
+  console.log(`${side.toUpperCase()} :: ${market.seq}`);
+  console.log(top5);
+});
+
+poloman.market('BTC_ETC');
+
+// 5 seconds later
+setTimeout(() => {
+  //poloman.disconnect();
+}, 5000);
